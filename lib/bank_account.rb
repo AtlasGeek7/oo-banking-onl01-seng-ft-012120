7c2c85dad7defe8504187ecb5f35d39992859207
Khalid T <khalid.tissire@gmail.com>
2020-02-08 11:30:17 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -4,33 +4,7 @@ class BankAccount
 
   def initialize(name)
     @name = name
-    @balance = 1000
-    @status = "open"
-  end
-
-  def deposit(deposit_amount)
-    self.balance += deposit_amount
-  end
-
-  def display_balance
-    "Your balance is $#{balance}."
-  end
-
-  def close_account
-    self.status = "closed"
-  end
-
-  def valid?
-    balance > 0 && status == "open"
-  end
-
-endclass BankAccount
-  attr_reader :name
-  attr_accessor :balance, :status
-
-  def initialize(name)
-    @name = name
-    @balance = 1000
+    @balance = 2000
     @status = "open"
   end
 
