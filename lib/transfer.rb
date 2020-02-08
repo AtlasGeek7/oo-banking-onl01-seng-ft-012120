8c5457524b3e28590a9ba1261ea39507986c93dc
Khalid T <khalid.tissire@gmail.com>
2020-02-08 11:34:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -35,6 +35,6 @@ class Transfer
 
   def reject_transfer
     self.status = "rejected"
-    "Transaction rejected. Please check your account balance."
+    return "Transaction rejected. Please check your account balance."
   end
 end
